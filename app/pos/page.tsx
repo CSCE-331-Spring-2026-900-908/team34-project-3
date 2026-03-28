79979c54aff47f294b37aa67204ecc88c38eb7ac
Ewan Smith <Ewan.smith_Tamu@tamu.edu>
2026-03-28 16:11:32 -0500

    test

--- app/pos/page.tsx
+++ app/pos/page.tsx
@@ -2,7 +2,7 @@ import { PosClient } from "@/components/pos-client";
 import { requireEmployeePage } from "@/lib/auth";
 import { getIngredientAddOns, getMenuItems } from "@/lib/db/menu-items";
 
-export default async function PosPage() {
+export default async function PosPage() { 
   const employee = await requireEmployeePage();
   const [menuItems, ingredients] = await Promise.all([getMenuItems(), getIngredientAddOns()]);
 
